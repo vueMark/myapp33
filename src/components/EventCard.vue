f296c02e3004ad270bd0e99b5ef981cdcde9e09b
mark.costales <mcostales@t2s-solutions.com>
2020-08-30 21:08:27 -0400

    middle of lesson 4 Vue Router Basics

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <h1></h1>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
